d86f5945396137ef10af31f2fb433dfca6215544
Husnutdinov Radmir <XTreme95@yandex.ru>
2016-02-05 23:40:12 +0300

    'end3list'

--- app/controllers/mat_stats_controller.rb
+++ app/controllers/mat_stats_controller.rb
@@ -31,9 +31,56 @@ class MatStatsController < ApplicationController
     require 'rubyXL'
 
     workbook = RubyXL::Workbook.new
+
+    #wtf???
+    if (!params[:files][:file1].nil?)
     sheet1(workbook)
-    sheet2(workbook)
-    sheet3(workbook)
+    end
+    if (!params[:files][:file2].nil?)
+      sheet2(workbook)
+    end
+    if (!params[:files][:file3].nil?)
+      sheet3(workbook)
+    end
+    if (!params[:files][:file4].nil?)
+      sheet4(workbook)
+    end
+    if (!params[:files][:file5].nil?)
+      sheet5(workbook)
+    end
+    if (!params[:files][:file6].nil?)
+      sheet6(workbook)
+    end
+    if (!params[:files][:file7].nil?)
+      sheet7(workbook)
+    end
+    if (!params[:files][:file8].nil?)
+      sheet8(workbook)
+    end
+    if (!params[:files][:file9].nil?)
+      sheet9(workbook)
+    end
+    if (!params[:files][:file10].nil?)
+      sheet10(workbook)
+    end
+    if (!params[:files][:file11].nil?)
+      sheet11(workbook)
+    end
+    if (!params[:files][:file12].nil?)
+      sheet12(workbook)
+    end
+    if (!params[:files][:file13].nil?)
+      sheet13(workbook)
+    end
+    if (!params[:files][:file14].nil?)
+      sheet14(workbook)
+    end
+    if (!params[:files][:file15].nil?)
+      sheet15(workbook)
+    end
+    if (!params[:files][:file16].nil?)
+      sheet16(workbook)
+    end
 
     workbook.write("#{current_user.email}.xlsx")
     send_file("#{current_user.email}.xlsx")
@@ -52,7 +99,7 @@ class MatStatsController < ApplicationController
     worksheet.add_cell(0,0, "Выборка")
     worksheet.change_column_width(1, 23)
     arr[0].each_with_index { |value, index|
-      worksheet.add_cell(index+1,0, value.to_f)
+      worksheet.add_cell(index+1,0, value.gsub(',','.').to_f)
     }
 
     worksheet.add_cell(1,1, 'Выборочное среднее = ')
@@ -97,7 +144,7 @@ class MatStatsController < ApplicationController
     #date
     arr[0].each_with_index { |value, index|
       if (index >2 )
-      worksheet.add_cell(index-2,0, value.to_f)
+      worksheet.add_cell(index-2,0, value.gsub(',','.').to_f)
       end
     }
 
@@ -139,7 +186,7 @@ class MatStatsController < ApplicationController
 
     (arr[0][2].to_i-1).times {
       worksheet.add_cell(count,5,'', "NORMDIST(D#{(count+1).to_s},H2,H4,FALSE)")
-
+     # worksheet.add_cell(count,4,'', "{=FREQUENCY(A:A,C:C)}")
     count+=1
     }
   end
@@ -153,6 +200,13 @@ class MatStatsController < ApplicationController
     worksheet = workbook.add_worksheet('Задание 3')
     worksheet.add_cell(0,0, "Выборка")
     worksheet.add_cell(0,1, "ЭФР")
+    worksheet.add_cell(0,2, "Норм. распр.")
+    worksheet.add_cell(0,3, "Расхождение")
+    worksheet.add_cell(0,4, "Макс. расхожд.")
+    worksheet.add_cell(0,5, "Среднее")
+    worksheet.add_cell(2,5, "Станд. отклон.")
+    worksheet.add_cell(1,5,'', 'AVERAGE(A:A)')
+    worksheet.add_cell(3,5,'', 'STDEV(A:A)')
 
     #date
     arr[0].sort!
@@ -160,10 +214,13 @@ class MatStatsController < ApplicationController
     #date
     arr[0].each_with_index { |value, index|
       if (index == 0)
-        worksheet.add_cell(index+1,0, value.to_f)
+        worksheet.add_cell(index+1,0, value.gsub(',','.').to_f)
+      elsif (index == (arr[0].length-1))
+      worksheet.add_cell(index+1+count,0, value.gsub(',','.').to_f)
+
       else
-        worksheet.add_cell(index+1+count,0, value.to_f)
-        worksheet.add_cell(index+1+(count+1),0, value.to_f)
+        worksheet.add_cell(index+1+count,0, value.gsub(',','.').to_f)
+        worksheet.add_cell(index+1+(count+1),0, value.gsub(',','.').to_f)
         count+=1
         end
     }
@@ -172,18 +229,184 @@ class MatStatsController < ApplicationController
     i=0
     count=arr[0].length*2
 
-    while i < count
+    while i < (count-2)
+      worksheet.add_cell(i+1,2,'', "NORMDIST(A#{(i+2).to_s},F2,F4,TRUE)")
+      worksheet.add_cell(i+1,3,'', "ABS(B#{i+2}-C#{i+2})")
       if (i == 0)
         worksheet.add_cell(1,1, 0)
       elsif(i%2==1)
-        worksheet.add_cell(i+1,1,'', "(ROW(B#{i.to_s})-1)/180-1/90")
+        worksheet.add_cell(i+1,1,'', "(ROW(B#{(i+2).to_s})-1)/#{(arr[0].length*2-4).to_s}-1/#{(arr[0].length-2).to_s}")
       else
-      worksheet.add_cell(i+1,1,'', "(ROW(B#{(i).to_s})-1)/180")
+      worksheet.add_cell(i+1,1,'', "(ROW(B#{(i+1).to_s})-1)/#{(arr[0].length*2-4).to_s}")
+      end
       i+=1
-      end
-      end
+    end
+    worksheet.add_cell(1,4,'', "MAX(D:D)")
+  end
+
+  private def sheet4(workbook)
+  arr = Array.new
+  uploaded_io = params[:files][:file4]
+  File.open(uploaded_io.open()) { |f|
+    arr.append(f.read.split())
+  }
+
+
+
+  worksheet = workbook.add_worksheet('Задание 4')
+
+  worksheet.add_cell(1,1, "#{arr[0][0]}")
+  arr[0].delete_at(0)
+  arr[0].each_with_index { |value, index|
+    worksheet.add_cell(index+1,0, value.gsub(',','.').to_f)
+  }
+
+  worksheet.add_cell(0,0, "Выборка")
+  worksheet.add_cell(0,1, "α")
+
+  worksheet.add_cell(2,1, "H0")
+  worksheet.add_cell(3,1, "нормальное")
+
+  worksheet.merge_cells(0, 2, 0, 3)
+  worksheet.merge_cells(3, 2, 4, 2)
+  worksheet.merge_cells(3, 3, 3, 4)
+  worksheet.add_cell(3,2, "границы")
+  worksheet.add_cell(3,3, "Частоты")
+  worksheet.add_cell(4,3, "выборочные")
+  worksheet.add_cell(4,4, "ожидаемые")
+  worksheet.add_cell(0, 2, 'Группированые')
+  worksheet.add_cell(1, 2, 'среднее')
+  worksheet.add_cell(2, 2, 'дисперсия')
+  worksheet.add_cell(1, 3, '','AVERAGE(A:A)')
+  worksheet.add_cell(2, 3, '','VARP(A:A)')
+
+  count = 1
+  arr[0][2].to_i.times {
+    worksheet.add_cell(count+4,2, arr[0][0].gsub(",",".").to_f+arr[0][1].gsub(",",".").to_f*(count-1))
+    count+=1
+  }
+
 
   end
 
+  private def sheet5(workbook)
+    arr = Array.new
+    uploaded_io = params[:files][:file3]
+    File.open(uploaded_io.open()) { |f|
+      arr.append(f.read.split())
+    }
+    worksheet = workbook.add_worksheet('Задание 5')
+    worksheet.add_cell(0,0, "Выборка")
+  end
+
+  private def sheet6(workbook)
+    arr = Array.new
+    uploaded_io = params[:files][:file3]
+    File.open(uploaded_io.open()) { |f|
+      arr.append(f.read.split())
+    }
+    worksheet = workbook.add_worksheet('Задание 6')
+    worksheet.add_cell(0,0, "Выборка")
+  end
+
+  private def sheet7(workbook)
+    arr = Array.new
+    uploaded_io = params[:files][:file3]
+    File.open(uploaded_io.open()) { |f|
+      arr.append(f.read.split())
+    }
+    worksheet = workbook.add_worksheet('Задание 7')
+    worksheet.add_cell(0,0, "Выборка")
+  end
+
+  private def sheet8(workbook)
+    arr = Array.new
+    uploaded_io = params[:files][:file3]
+    File.open(uploaded_io.open()) { |f|
+      arr.append(f.read.split())
+    }
+    worksheet = workbook.add_worksheet('Задание 8')
+    worksheet.add_cell(0,0, "Выборка")
+  end
+
+  private def sheet9(workbook)
+    arr = Array.new
+    uploaded_io = params[:files][:file3]
+    File.open(uploaded_io.open()) { |f|
+      arr.append(f.read.split())
+    }
+    worksheet = workbook.add_worksheet('Задание 9')
+    worksheet.add_cell(0,0, "Выборка")
+  end
+
+  private def sheet10(workbook)
+    arr = Array.new
+    uploaded_io = params[:files][:file3]
+    File.open(uploaded_io.open()) { |f|
+      arr.append(f.read.split())
+    }
+    worksheet = workbook.add_worksheet('Задание 10')
+    worksheet.add_cell(0,0, "Выборка")
+  end
+
+  private def sheet11(workbook)
+    arr = Array.new
+    uploaded_io = params[:files][:file3]
+    File.open(uploaded_io.open()) { |f|
+      arr.append(f.read.split())
+    }
+    worksheet = workbook.add_worksheet('Задание 11')
+    worksheet.add_cell(0,0, "Выборка")
+  end
+
+  private def sheet12(workbook)
+    arr = Array.new
+    uploaded_io = params[:files][:file3]
+    File.open(uploaded_io.open()) { |f|
+      arr.append(f.read.split())
+    }
+    worksheet = workbook.add_worksheet('Задание 12')
+    worksheet.add_cell(0,0, "Выборка")
+  end
+
+  private def sheet13(workbook)
+    arr = Array.new
+    uploaded_io = params[:files][:file3]
+    File.open(uploaded_io.open()) { |f|
+      arr.append(f.read.split())
+    }
+    worksheet = workbook.add_worksheet('Задание 13')
+    worksheet.add_cell(0,0, "Выборка")
+  end
+
+  private def sheet14(workbook)
+    arr = Array.new
+    uploaded_io = params[:files][:file3]
+    File.open(uploaded_io.open()) { |f|
+      arr.append(f.read.split())
+    }
+    worksheet = workbook.add_worksheet('Задание 14')
+    worksheet.add_cell(0,0, "Выборка")
+  end
+
+  private def sheet15(workbook)
+    arr = Array.new
+    uploaded_io = params[:files][:file3]
+    File.open(uploaded_io.open()) { |f|
+      arr.append(f.read.split())
+    }
+    worksheet = workbook.add_worksheet('Задание 15')
+    worksheet.add_cell(0,0, "Выборка")
+  end
+
+  private def sheet16(workbook)
+    arr = Array.new
+    uploaded_io = params[:files][:file3]
+    File.open(uploaded_io.open()) { |f|
+      arr.append(f.read.split())
+    }
+    worksheet = workbook.add_worksheet('Задание 16')
+    worksheet.add_cell(0,0, "Выборка")
+  end
 
 end
